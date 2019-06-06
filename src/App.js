0b61fa8a956ab00be671f66adc0379d2273c7626
Michael <mobrien@colgate.edu>
2019-06-06 17:17:22 -0400

    basic hook working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <p>You clicked {count} times!</p>
+      <button onClick={() => setCount(count + 1)}>Increase count</button>
     </div>
   );
 }
